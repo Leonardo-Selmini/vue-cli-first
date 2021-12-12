b8b31866ecf04fb7b3620561839aa52775e4df5f
Leonardo.Selmini <selminileonardo@gmail.com>
2021-12-13 00:02:08 +0100

    component title

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
+    <Title/>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Title from "./components/Title.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Title
+    }
   }
-}
 </script>
 
 <style lang="scss">
